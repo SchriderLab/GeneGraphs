52e2484a029b04ac402a6a4891c16e130957b2e8
Nick Matthew <nick.matthew414@gmail.com>
2021-01-21 14:50:37 -0500

    condensed script

--- make_simulations.sh
+++ make_simulations.sh
@@ -4,12 +4,17 @@ main()
 {
     replicates=$1
     outdir=$2
-    ofile=$3
+    real=$3
     shift; shift; shift;
     for arg in "$@"; do
         python src/data/simulate_msprime.py --outdir "${outdir}/${arg}" --id test --length 0.1 --model $arg --replicates $replicates
     done
-    python3 src/data/format.py --idir $outdir --ofile "${ofile}.hdf5"
+    if [ $real == "real" ]
+    then
+        python3 src/data/format.py --idir $outdir --ofile "${outdir}.hdf5" --real 
+    else
+        python3 src/data/format.py --idir $outdir --ofile "${outdir}.hdf5"
+    fi
     echo "done with formatting!"
 }
 
