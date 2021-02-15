a494b8af67a3f8cb61ccedd88245b4a4d674f372
Nick Matthew <nick.matthew414@gmail.com>
2021-02-15 18:27:52 -0500

    update autoencoder slurm script

--- slurm/run_autoencoder.sh
+++ slurm/run_autoencoder.sh
@@ -13,8 +13,8 @@
 #SBATCH --job-name=pytorch
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1
-#SBATCH --mem=32G
-#SBATCH --time=2-00:00:00
+#SBATCH --mem=64g
+#SBATCH --time=5-00:00:00
 #SBATCH --partition=volta-gpu
 #SBATCH --output=run-%j.log
 #SBATCH --gres=gpu:1
@@ -24,31 +24,18 @@
 
 unset OMP_NUM_THREADS
 
-# Set SIMG path
-SIMG_PATH=/nas/longleaf/apps/pytorch_py3/1.4.0/simg
+SIMG_NAME=/proj/dschridelab/SparseNets/pytorch1.4.0-py3-cuda10.1-ubuntu16.04_production.simg
 
-# Set SIMG name
-SIMG_NAME=pytorch1.4.0-py3-cuda10.1-ubuntu16.04.simg
 
 IFILE=$1
 IFILE_VAL=$2
 ODIR=$3
 N_EPOCHS=$4
-IN_FEATURES=$5
-OUT_FEATURES=$6
-LINEAR=$7
-VARIATIONAL=$8
-TAG=$9
-ADVERSARIAL=${10}
+CONFIG=$5
+TAG=test
 
 mkdir -p ${ODIR}
 
 # GPU with Singularity
-if [ ADVERSARIAL ]
-then
-    echo singularity exec --nv -B /pine -B /proj $SIMG_PATH/$SIMG_NAME python3 ../src/models/train_adversarial_autoencoder.py  --ifile ${IFILE} --ifile_val ${IFILE_VAL} --odir ${ODIR} --n_epochs ${N_EPOCHS} --in_features ${IN_FEATURES} --out_features ${OUT_FEATURES} --linear ${LINEAR} --variational ${VARIATIONAL} --tag ${TAG} --verbose
-    singularity exec --nv -B /pine -B /proj $SIMG_PATH/$SIMG_NAME python3 ../src/models/train_adversarial_autoencoder.py --ifile ${IFILE} --ifile_val ${IFILE_VAL} --odir ${ODIR} --n_epochs ${N_EPOCHS} --in_features ${IN_FEATURES} --out_features ${OUT_FEATURES} --linear ${LINEAR} --variational ${VARIATIONAL} --tag ${TAG} --verbose
-else
-    echo singularity exec --nv -B /pine -B /proj $SIMG_PATH/$SIMG_NAME python3 ../src/models/train_autoencoder.py  --ifile ${IFILE} --ifile_val ${IFILE_VAL} --odir ${ODIR} --n_epochs ${N_EPOCHS} --in_features ${IN_FEATURES} --out_features ${OUT_FEATURES} --linear ${LINEAR} --variational ${VARIATIONAL} --tag ${TAG} --verbose
-    singularity exec --nv -B /pine -B /proj $SIMG_PATH/$SIMG_NAME python3 ../src/models/train_autoencoder.py --ifile ${IFILE} --ifile_val ${IFILE_VAL} --odir ${ODIR} --n_epochs ${N_EPOCHS} --in_features ${IN_FEATURES} --out_features ${OUT_FEATURES} --linear ${LINEAR} --variational ${VARIATIONAL} --tag ${TAG} --verbose
-fi
+  echo singularity exec --nv -B /pine -B /proj $SIMG_NAME python3 ../src/models/train_autoencoder.py  --ifile ${IFILE} --ifile_val ${IFILE_VAL} --odir ${ODIR} --n_epochs ${N_EPOCHS} --config ${CONFIG} --tag ${TAG} --verbose
+  singularity exec --nv -B /pine -B /proj $SIMG_NAME python3 ../src/models/train_autoencoder.py --ifile ${IFILE} --ifile_val ${IFILE_VAL} --odir ${ODIR} --n_epochs ${N_EPOCHS} --config ${CONFIG} --tag ${TAG} --verbose
