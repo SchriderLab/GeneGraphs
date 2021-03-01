50e3cc02864413f7bc9e9716f6fc62220341b1f1
Logan Whitehouse <lswhiteh@live.unc.edu>
2021-03-01 11:33:49 -0500

    Created slurm script to convert all trees to embeddings NPZ file.

---
+++ embeddings/slurm/run_embedding_conversion.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#SBATCH -p general
+#SBATCH --nodes=1
+#SBATCH --time=12:00:00
+#SBATCH --mem=8G
+#SBATCH --ntasks=4
+#SBATCH -J embeddings
+#SBATCH -o embeddings.%A.out
+#SBATCH -e embeddings.%A.err
+##SBATCH --mail-type=BEGIN,END,FAIL
+##SBATCH --mail-user=lswhiteh@email.unc.edu
+
+#Replace these with default examples before release
+cd /overflow/dschridelab/projects/GeneGraphs/embeddings
+source activate compbio
+
+python convert_data.py
