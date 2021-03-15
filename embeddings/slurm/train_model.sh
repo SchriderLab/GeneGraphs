e0a83af029fe3040191a4f64d58e5d521a9dfa26
Logan Whitehouse <lswhiteh@live.unc.edu>
2021-03-15 14:28:04 -0400

    Script for training models on slurm.

---
+++ embeddings/slurm/train_model.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#SBATCH --time=48:00:00
+#SBATCH -p volta-gpu
+#SBATCH --qos=gpu_access 
+#SBATCH --gres=gpu:2
+#SBATCH --mem=64G
+#SBATCH -J train_newrep
+#SBATCH -o train_newrep.%A.out
+#SBATCH -e train_newrep.%A.err
+#SBATCH --mail-type=BEGIN,END,FAIL
+#SBATCH --mail-user=lswhiteh@email.unc.edu
+
+cd /overflow/dschridelab/projects/GeneGraphs/embeddings
+source activate compbio
+
+python tree_network.py lstm
